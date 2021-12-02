2b5b183e918c9eb5c18d8dc74e765b2e99e14107
chirag0002 <varshneychirag34@gmail.com>
2021-12-02 20:56:48 +0530

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react" 
+import "../styling/header.css"
+
+
+function Header() {
+    return (
+        <div className = "App" >
+            <h1>hello</h1>
+        </div>
+    );
+}
+
+export default Header;
