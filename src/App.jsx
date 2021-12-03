3f2a7f65698203a8effdeb02c65f4526dc0c9354
Bhavya <bhavya.sachdeva.58@gmail.com>
2021-12-03 19:23:08 +0530

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header';
+import Home_page from './Components/Home_page';
 
 
 function App() {
     return ( <div className = "App" >
-        <Header/>
+        <Header/>,
+        <Home_page />
 
         
         </div>
---
+++ src/Components/Home_page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../styling/home_page.css"
+
+const Home_page = () => {
+    return (
+        <div>
+            <div className="container_5">
+                <section className="cont_5">
+ME
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default Home_page
