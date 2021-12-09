2ae72bb7ae8185f79679ab4e7b02f8d50792b827
kanika637 <kanikagola26@gmail.com>
2021-12-09 17:18:34 +0530

    pushing

--- src/Components/Container1.js
+++ src/Components/Container1.js
@@ -3,12 +3,20 @@ import "../styling/container1.css"
 import SearchIcon from '@mui/icons-material/Search';
 import logo from "../Images/logo.png"
 
-const Container1 = () => {
+const Container1 = ({props}) => {
     return (
         <div className="container1">
              <img className="logo" src={logo} />
             <div className="header">
-                
+                <div className="search_box">
+                    <input type="text" placeholder="Type to search"></input>
+                    <div className="search_button">
+                        <i className="fas fa-search"></i>
+                    </div>
+                    <div className="cancel_button">
+                        <i className="fas fa-times"></i>
+                    </div>
+                </div>
                
                 
                 <img className="back_img" src="https://blog.bonus.ly/hubfs/workplace-conflict.png"/>
@@ -21,10 +29,9 @@ const Container1 = () => {
                 
             </div>
             <div className="search_bar">
-                <div className="input">
-                    <input  className="bar" type="text" placeholder="Search your future"></input>
-                    <div className="search_icon"><SearchIcon></SearchIcon></div>
-                </div>
+
+            
+                
             </div>
             <div className="side_stuff">
                 <div className="first">About Us</div>
