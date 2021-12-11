19e538369c5f4923f6736d03bef4ec45e421bb01
Aayush's Mac <SuperAayush@1234s-MacBook-Air.local>
2021-12-11 19:32:37 +0530

    Neat Code

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,21 +3,17 @@ import "../styling/login.css"
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import pic from "../Images/idea_concepts.png"
- import bg from "../Images/bg.png"
+
 const Login = () => {
     return (
 
         <div className= "cont">
-        {/* <img className="bg" src={bg} alt=""/> */}
                 <img className="picture" src={pic} alt="" />
             <Form class="form">
             
   <Form.Group  controlId="formBasicEmail">
     <Form.Label className="user"> Username or Email </Form.Label>
     <Form.Control className="email-1" type="email" />
-    {/* <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text> */}
   </Form.Group>
 
   <Form.Group controlId="formBasicPassword">
