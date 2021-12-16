831a57607bbf4e31dd1f73631b0d07cf218bfa6b
kanika637 <kanikagola26@gmail.com>
2021-12-16 14:25:42 +0530

    Query backend in process

--- Server/controllers/PathLogic.js
+++ Server/controllers/PathLogic.js
@@ -1,3 +1,26 @@
 export const getHomePage =(req, res) => {
     res.send("Changed");
 }
+
+export const getQuery=async (req,res)=>{
+    const{email,phone,query}=req.body;
+
+    if(!email|| !phone|| !query){
+        return res.status(422).json({error:"Please enter valid details"})
+    }
+    try{
+        const userExist=await User.findOne({email:email});
+        if(userExist){
+            return res.status(422).json({error:"Email already exist"});
+        }
+        else{
+            const user=new user({email,phone,query});
+            await user.save();
+            res.status(301).json({message:"Query noted"});
+        }
+    }
+    catch(error){
+        console.log(error);
+
+    }
+}
